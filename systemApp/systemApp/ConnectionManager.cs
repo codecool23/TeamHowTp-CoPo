f5914f2189ca94554b106cb4b4fda0180b9353ba
Judit <tolnai.judit12@gmail.com>
2017-09-07 16:53:12 +0200

    TemporaryConnection class ConnectionsManager class dynamic table generation info view added

---
+++ systemApp/systemApp/ConnectionManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace systemApp
+{
+    public class ConnectionManager
+    {
+        public static List<Models.TemporaryConnection> connectionList = new List<Models.TemporaryConnection>
+        {
+           new Models.TemporaryConnection(){IP = "192.168.161.133", Name = "John"},
+           new Models.TemporaryConnection(){IP = "192.168.161.134", Name = "George"},
+           new Models.TemporaryConnection(){IP = "192.168.161.135", Name = "Paul"},
+           new Models.TemporaryConnection(){IP = "192.168.161.136", Name = "Ringo"},
+        };
+    }
+}
