47340d2e70392ef976b88f4baedb4a72d723dc16
Judit <tolnai.judit12@gmail.com>
2017-09-08 14:57:57 +0200

    Debug and bin folder untracked

--- systemApp/systemApp/ConnectionManager.cs
+++ systemApp/systemApp/ConnectionManager.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.NetworkInformation;
+using System.IO;
 
 namespace systemApp
 {
