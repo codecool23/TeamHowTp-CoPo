083b36f09a25e3bb6006cb497832661feb47cd16
Amador99 <abelofvarad@gmail.com>
2017-09-11 17:29:17 +0200

    Implemented Wcf Discovery prototype.. again.

---
+++ systemApp/WcfDiscoveryClient/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Discovery;
+using System.Text;
+using System.Threading.Tasks;
+using WcfLibrary;
+
+namespace WcfDiscoveryClient
+{
+    class Program
+    {
+        class WcfClient
+        {
+            public static void Main(string[] args)
+            {
+                System.Threading.Thread.Sleep(4000);
+                WcfClient_SetupChannel();
+                System.Threading.Thread.Sleep(2000);
+                WcfClient_Ping();
+            }
+
+            private static IWcfTestMessage channel;
+
+            public static Uri WcfClient_DiscoverChannel()
+            {
+                var discoveryclient = new DiscoveryClient(new UdpDiscoveryEndpoint());
+                FindCriteria findcriteria = new FindCriteria(typeof(IWcfTestMessage));
+                findcriteria.Duration = TimeSpan.FromSeconds(5);
+                FindResponse findresponse = discoveryclient.Find(findcriteria);
+                foreach (EndpointDiscoveryMetadata edm in findresponse.Endpoints)
+                {
+                    Console.WriteLine("uri found = " + edm.Address.Uri.ToString());
+                }
+                return findresponse.Endpoints[0].Address.Uri;
+            }
+
+            public static void WcfClient_SetupChannel()
+            {
+                var binding = new BasicHttpBinding(BasicHttpSecurityMode.None);
+                var factory = new ChannelFactory<IWcfTestMessage>(binding);
+                var uri = WcfClient_DiscoverChannel();
+                Console.WriteLine("creating channel to " + uri.ToString());
+                EndpointAddress ea = new EndpointAddress(uri);
+                channel = factory.CreateChannel(ea);
+                Console.WriteLine("channel created");
+            }
+
+            public static void WcfClient_Ping()
+            {
+                Console.WriteLine("getting message from host");
+                string result = channel.Message();
+                Console.WriteLine("message result = " + result);
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
---
+++ systemApp/WcfDiscoveryHost/WcfHost.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Discovery;
+using System.ServiceModel.Description;
+using System.Net;
+using WcfLibrary;
+
+namespace WcfDiscoveryHost
+{
+
+    class WcfHost
+    {
+        public static void Main(string[] args)
+        {
+            WcfHost_Open();
+            Console.ReadKey();
+        }
+
+        public static void WcfHost_Open()
+        {
+            string hostname = Dns.GetHostEntry(Dns.GetHostName()).AddressList.First(f => f.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork).ToString();
+            var baseAddress = new UriBuilder("http", hostname, 2000, "WcfTestMessage");
+            var host = new ServiceHost(typeof(WcfTestMessage), baseAddress.Uri);
+
+            // enable processing of discovery messages.  use UdpDiscoveryEndpoint to enable listening. use EndpointDiscoveryBehavior for fine control.
+            host.Description.Behaviors.Add(new ServiceDiscoveryBehavior());
+            host.AddServiceEndpoint(new UdpDiscoveryEndpoint());
+
+            // enable wsdl, so you can use the service from WcfStorm, or other tools.
+            var smb = new ServiceMetadataBehavior();
+            smb.HttpGetEnabled = true;
+            smb.MetadataExporter.PolicyVersion = PolicyVersion.Policy15;
+            host.Description.Behaviors.Add(smb);
+
+            // create endpoint
+            var binding = new BasicHttpBinding(BasicHttpSecurityMode.None);
+            binding.MaxReceivedMessageSize = 4294967295;
+            binding.TransferMode = TransferMode.Streamed;
+            host.AddServiceEndpoint(typeof(IWcfTestMessage), binding, "");
+            host.Open();
+            Console.WriteLine("host open at " + baseAddress);
+        }
+    }
+}
