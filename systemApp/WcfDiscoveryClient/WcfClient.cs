0fce29b8120924e872991b4487cfbb330561f1c7
Amador99 <abelofvarad@gmail.com>
2017-09-11 17:35:50 +0200

    Renamed a few things.. minor changes

--- systemApp/WcfDiscoveryClient/WcfClient.cs
+++ systemApp/WcfDiscoveryClient/WcfClient.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.ServiceModel;
 using System.ServiceModel.Discovery;
-using System.Text;
-using System.Threading.Tasks;
 using WcfLibrary;
 
 namespace WcfDiscoveryClient
