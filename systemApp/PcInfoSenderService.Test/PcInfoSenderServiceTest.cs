292b7b7c157393d50c9b059ca94d7172b2bdee40
Amador99 <abelofvarad@gmail.com>
2017-09-18 12:15:37 +0200

    First tests for PcInfoSenderService class implemented

---
+++ systemApp/PcInfoSenderService.Test/PcInfoSenderServiceTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace PcInfoSenderService.Test
+{
+    [TestFixture]
+    public class PcInfoSenderServiceTest
+    {
+        IPcInfoSender sut;
+
+        [OneTimeSetUp]
+        public void TestSetup()
+        {
+            sut = new PcInfoSender();
+        }
+
+        [Test]
+        public void ShouldGetRuntimeInformation()
+        {
+            Assert.That(sut.GetRuntimeInformation(), Is.Not.Null);
+        }
+
+        [Test]
+        public void ShouldGetDeviceInformation()
+        {
+            Assert.That(sut.GetDeviceInformation(), Is.Not.Null);
+        }
+
+        [OneTimeTearDown]
+        public void TestTearDown()
+        {
+            sut = null;
+        }
+    }
+}
