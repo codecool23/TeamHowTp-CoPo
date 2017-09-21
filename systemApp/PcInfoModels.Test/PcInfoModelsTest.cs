9a7a4e6723384dafd707fb918e053adc49501c3b
Amador99 <abelofvarad@gmail.com>
2017-09-21 10:39:10 +0200

    Added two more unit tests

--- systemApp/PcInfoModels.Test/PcInfoModelsTest.cs
+++ systemApp/PcInfoModels.Test/PcInfoModelsTest.cs
@@ -4,16 +4,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using PcInfoModels;
 
 namespace PcInfoModels.Test
 {
-    [TestFixture]
+    [TestFixture] 
     public class PcInfoModelsTest
     {
+        RuntimeInfo runTimeInfo;
+
         [OneTimeSetUp]
         public void TestSetup()
         {
+           runTimeInfo = new RuntimeInfo();
+        }
+
+        [Test]
+        public void ShouldDiskSpaceCountSpaces()
+        {
+            DiskSpace.CountSpaces();
+            Assert.That(DiskSpace.AllDisk.Count(), Is.GreaterThan(0));
+        }
+
+        [Test]
+        public void ShouldRuntimeInfoGetWindowsInstallationDateTime()
+        {
+            DateTime testDate = new DateTime(1984, 1, 1, 0, 0, 0);
+            Assert.True(runTimeInfo.InstallDate > testDate );
             
+        }
+
+        [OneTimeTearDown]
+        public void TestTearDown()
+        {
+            runTimeInfo = null;
         }
     }
 }
