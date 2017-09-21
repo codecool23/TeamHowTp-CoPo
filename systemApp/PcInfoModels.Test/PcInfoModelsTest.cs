d9f82ac662f42aa9289a7e26cd3f35b28fd774d8
Amador99 <abelofvarad@gmail.com>
2017-09-21 09:59:23 +0200

    Setting up PcInfoModels testing

---
+++ systemApp/PcInfoModels.Test/PcInfoModelsTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace PcInfoModels.Test
+{
+    [TestFixture]
+    public class PcInfoModelsTest
+    {
+        [OneTimeSetUp]
+        public void TestSetup()
+        {
+            
+        }
+    }
+}
