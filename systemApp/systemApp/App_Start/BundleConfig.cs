3c8dd6f3ee0587669be7d539eeac0a4595039e55
okocsis90 <o.kocsis90@gmail.com>
2017-09-21 11:59:30 +0200

    HOTFIX/okocsis90_download_outsourced_into_a_js_file

--- systemApp/systemApp/App_Start/BundleConfig.cs
+++ systemApp/systemApp/App_Start/BundleConfig.cs
@@ -23,6 +23,9 @@ namespace systemApp
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/download").Include(
+                      "~/Scripts/download.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
